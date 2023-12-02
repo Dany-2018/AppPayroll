6f85a9ca3565beef4a757cba3fce5d021f2c4b71
Dany Ramirez <dany.asrock@gmail.com>
2023-12-01 19:42:47 -0500

    Agregar archivos de proyecto.

---
+++ AppPayroll/Controllers/HomeController.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Text.Json;
+using AppPayroll.Models;
+using System.Reflection;
+using System.Threading;
+
+namespace AppPayroll.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public async Task<ActionResult> Index()
+        {
+            var response = await GetEmployeesFromApi();
+            Thread.Sleep(5000);
+
+            if (response.Data == null )
+            {
+                return View("Index");
+            }
+            else
+            {
+                return View(response?.Data);
+            }
+        }
+
+        private async Task<ApiResponse> GetEmployeesFromApi()
+        {
+            var httpClient = new HttpClient();
+            var obj = await httpClient.GetStringAsync("http://dummy.restapiexample.com/api/v1/employees");
+            return JsonConvert.DeserializeObject<ApiResponse>(obj);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        // Método para obtener los detalles del empleado por ID desde una API externa
+        public async Task<ActionResult> MostrarEmpleadoPorId(int id)
+        {
+            try
+            {
+                if (id == 0 )
+                {
+                    return View("Index");
+                }
+
+                Thread.Sleep(10000);
+                // URL de la API que proporciona detalles del empleado por ID
+                string apiUrl = $"https://dummy.restapiexample.com/api/v1/employee/{id}";
+
+
+                await Task.Delay(2000);
+                var httpClient = new HttpClient();
+                await Task.Delay(2000);
+                var response = await httpClient.GetStringAsync(apiUrl);
+
+            
+
+                ApiResponseSearch resp = JsonConvert.DeserializeObject<ApiResponseSearch>(response);
+
+                if (resp.Data == null || resp.Status != "success"  )
+                {
+                    // Manejo cuando el ID es incorrecto o el estado de la respuesta no es "success"
+                    return View("Index");
+                }
+                else
+                {
+                    return View(resp?.Data);
+                }
+            }
+            catch (HttpRequestException)
+            {
+                // Manejo de errores de solicitud HTTP
+                return View("Index");
+            }
+            catch (Newtonsoft.Json.JsonException)
+            {
+                // Manejo de errores de deserialización JSON
+                return View("Index");
+            }
+            catch (Exception)
+            {
+                // Otros errores
+                return View("Index");
+            }
+
+        }
+
+    }
+
+}
---
+++ AppPayroll/Models/Employee.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppPayroll.Models
+{
+    public class Employee
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("employee_name")]
+        public string EmployeeName { get; set; }
+
+        [JsonProperty("employee_salary")]
+        public decimal EmployeeSalary { get; set; }
+
+        [JsonProperty("employee_age")]
+        public int EmployeeAge { get; set; }
+        // Otros campos del empleado, si los hay
+
+        // Propiedad calculada para el salario anual
+        public decimal SalarioAnual => EmployeeSalary * 12;
+    }
+
+    public class ApiResponse
+    {
+        public List<Employee> Data { get; set; }
+    }
+
+    public class ApiResponseSearch
+    {
+        public string Status { get; set; }
+        public Employee Data { get; set; }
+    }
+}
